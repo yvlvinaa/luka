8bfa272ff41aad663b5761a1b256bdc362234c47
yvlvinaa <morenoyeleina@gmail.com>
2026-05-24 13:14:09 -0400

    Add files via upload

---
+++ cards (1).py
@@ -0,0 +1,264 @@
+cards = [
+    # ⭐⭐⭐ 3 STAR (RARE)
+    {
+        "name": "Gojo",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://cdn.discordapp.com/attachments/1482753957301456907/1507756783769223229/15_sin_titulo_20260523164701.png?ex=6a130f6e&is=6a11bdee&hm=92a45a4d5bc9cd6bc8fe1654a0dd13f4e9b6888c09753f72d07c9c7cd1f4dd3e"
+    },
+    {
+        "name": "Jabber Wonger",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083168287264829/Untitled369_20260524200532.png?ex=6a143f66&is=6a12ede6&hm=fe5d2ca18f11e95333ae19490769ce642d5df4fa29ae1973a53ef3cc057fa01a&"
+    },
+    {
+        "name": "Castorice",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508151395403043017/Untitled129_20260524125435.png?ex=6a147ef0&is=6a132d70&hm=637320915d7055cce9a2fc568fdfc0b425e0d1e86f751aab781745ab388d7702&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Blade",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508151396103356547/Untitled129_20260524125335.png?ex=6a147ef0&is=6a132d70&hm=abc6bda4ddc7cf213f6b6f97e99fe4918a78c2edd80b67ac26105bdb1a2f6c4e&=&format=webp&quality=lossless&width=559&height=745"
+    },
+
+    {
+        "name": "Mydei",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://media.discordapp.net/attachments/1380934027544105040/1508083308435734618/Untitled359_20260523223351.png?ex=6a143f87&is=6a12ee07&hm=33b8958948cbf50800336e66247ed94677a3745b21d1824cf94736213a5f8852&=&format=webp&quality=lossless&width=559&height=745"
+    },
+
+    {
+        "name": "Zanka Nijiku",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083190114549941/Untitled369_20260524200305.png?ex=6a143f6b&is=6a12edeb&hm=a824cf2001ae3d1a33acfd96842619e3fd4709c5faf2718fd2995319d6649589&"
+    },
+
+    {
+        "name": "Hikaru",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083192358506537/Untitled369_20260524195427.png?ex=6a143f6b&is=6a12edeb&hm=d6df8ba62b3bee7c194b93ffb64aa337ef88b7ffb548b1772343ea5fe6ffd39b&"
+    },
+    {
+        "name": "Till",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803086662537247/Untitled357_20260523221554.png?ex=6a133a8d&is=6a11e90d&hm=639097bddf7850719c5dde6f887a9c3b481d8e8e0dc6e01a120767735db2b1e8&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Dewey",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083252836044973/Untitled362_20260524083722.png?ex=6a143f7a&is=6a12edfa&hm=4e681ad94253744562268112a0f3175a1f4798864c4f504b13a979e1eda0792f&"
+    },
+
+    {
+        "name": "Seishiro Nagi",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507867303679889599/16_sin_titulo_20260524000611.png?ex=6a13765c&is=6a1224dc&hm=a2242ba7ee138ff6bd2f6766767756791068f6a93583f7d6f439af09dbe10af8&=&format=webp&quality=lossless&width=411&height=547"
+    },
+
+
+    # ⭐⭐ 2 STAR (COMMON-MID)
+    {
+        "name": "Ivan",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803254061531317/Untitled357_20260523221830.png?ex=6a133ab5&is=6a11e935&hm=0f0efe643ed087975b117b4c4b95a7972c8201ce89c1e2ad01275ff298f7b999&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Luka",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803253797159114/Untitled357_20260523221839.png?ex=6a133ab5&is=6a11e935&hm=25d72969c790151c42a8ff8e4e9b4eaa1415f5d836e9a862f0f8e356fcd41727&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Tsukasa Tenma",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508146934303555674/Untitled131_20260524123720.png?ex=6a147ac9&is=6a132949&hm=1241229128c95a35e43bf2909043bcefb1343b47588075c23bb039b016a7ae02&=&format=webp&quality=lossless&width=681&height=908"
+    },
+    {
+        "name": "Shishikuno Mogari",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508148134138613780/16_sin_titulo_20260524184203.png?ex=6a147be7&is=6a132a67&hm=8cadd0855000f33508bc2a42411b5425917d63a282ec7a29174f8f9b347815aa&=&format=webp&quality=lossless&width=411&height=547"
+    },
+    {
+        "name": "Clorinde",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508152338655744202/Untitled132_20260524125656.png?ex=6a147fd1&is=6a132e51&hm=b550c70443bbdec4bf1b1bd8b58693ecb0d9c9fe33bd1c36f14375fa29932854&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Enjin",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083164264792104/Untitled369_20260524201143.png?ex=6a143f65&is=6a12ede5&hm=d8ee6e453fb72c54fd8a158e256910c61e86aecc82839915a86ee564abee23aa&"
+    },
+    {
+        "name": "Hinomori Shizuku",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508149813424689162/Untitled131_20260524124848.png?ex=6a147d77&is=6a132bf7&hm=1544be620eb3174315d6a62c32fda44d6df3c119ab70e83e4d484efe67087e0f&=&format=webp&quality=lossless&width=681&height=908"
+    },
+
+
+    {
+        "name": "Lee Yeon",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://cdn.discordapp.com/attachments/997404068517515324/1508039863809609819/16_sin_titulo_20260524104433.png?ex=6a141711&is=6a12c591&hm=06737cc14b6b2b0f5f53f75e5c3edbfd785d952c24dda53a665440fbbefec35d&"
+    },
+    {
+        "name": "Hong Kihoon",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1508039864317247538/16_sin_titulo_20260524105256.png?ex=6a141711&is=6a12c591&hm=c09e3ee6d843c30b65abcc2bc6d2541e12eac860d8b3c8013b0963630c2473fa&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Hyuna",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803252677410937/Untitled357_20260523221932.png?ex=6a133ab5&is=6a11e935&hm=6af9075eb6f6927f6b1fadd2e972d3e59b203e4f3c6676fdddc0af36abedac64&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Sua",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803253302362234/Untitled357_20260523221904.png?ex=6a133ab5&is=6a11e935&hm=c7dfc3fe7437c11b5d67c0de0f6d497aea95543760ec5b30d2826064a83a7968&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Suguru Geto",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507871934136844288/16_sin_titulo_20260523235608.png?ex=6a137aac&is=6a12292c&hm=81e6a0ad27a66b8ac6c7b0e93fa8cf7eccd57a97baa5766e78688e76305a62fd&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Choso",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507871945931231283/16_sin_titulo_20260524000923.png?ex=6a137aae&is=6a12292e&hm=cc7d099a6effc10d4b97ca82ab32e706c19e41753e06d9e5b09061431d919bc7&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Zanka Nijiku",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1380934027544105040/1508083190114549941/Untitled369_20260524200305.png?ex=6a143f6b&is=6a12edeb&hm=a824cf2001ae3d1a33acfd96842619e3fd4709c5faf2718fd2995319d6649589&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Shinonome Akito",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507871934673715280/16_sin_titulo_20260524000207.png?ex=6a137aac&is=6a12292c&hm=36da781c33a03dfc0749ccde1fc5081323451397d172255b71b7e1105446154c&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Yoshiki",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://cdn.discordapp.com/attachments/1380934027544105040/1508083193402753186/Untitled369_20260524195328.png?ex=6a143f6c&is=6a12edec&hm=03d73fd00bbee99202fa4cc991f3fb684de0939c4339cb9caa225d6138dfa254&"
+    },
+    {
+        "name": "Tartaglia",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508152339234553927/Untitled132_20260524125748.png?ex=6a147fd1&is=6a132e51&hm=40ec20c832bfb3e5a4764b06bfde9dfdd4acf23a4c4c1f5a1cb5c4b3cc433dc6&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Itoshi Sae",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507869425607643186/Untitled130_20260523181412.png?ex=6a137855&is=6a1226d5&hm=229d1ceaab974e7eb5fd5b33cb7fb740c6cee61028675ccba10d4456ca973318&=&format=webp&quality=lossless&width=681&height=908"
+    },
+
+
+
+
+
+    # ⭐ 1 STAR (COMMON)
+    {
+        "name": "Mydei",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1482753957301456907/1507753676150669412/Untitled359_20260523223439.png?ex=6a130c89&is=6a11bb09&hm=7629c4927f8b375b6597ad1bd415b4e158856ccad8b74b553982434b9b2b5714&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Mizi",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507803179302256810/Untitled357_20260523222147.png?ex=6a133aa3&is=6a11e923&hm=1245a720ad788d6c44ba2d9843a270d8be0dc3fa36eb16a80682ca1ff035b19b&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Riyo Reaper",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1380934027544105040/1508083164910977074/Untitled369_20260524200911.png?ex=6a143f65&is=6a12ede5&hm=7b2fdc53da54d04b69642ae6f44636e59961f0efdf409866d1627ca6df3c5bd1&=&format=webp&quality=lossless&width=559&height=745"
+    },
+
+    {
+        "name": "Meguru Bachira",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507870144406356100/16_sin_titulo_20260524001731.png?ex=6a137901&is=6a122781&hm=14f85e691dedbc2603520346e36537f2fd15e376703b4bfb1aba4f6577c235d5&=&format=webp&quality=lossless&width=681&height=908"
+    },
+    {
+        "name": "Hyoma Chigiri",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507869348889366581/16_sin_titulo_20260524001335.png?ex=6a137843&is=6a1226c3&hm=26a11e7ef983fca072368726702f1e84c76947183193d3d99d177ab338c06a51&=&format=webp&quality=lossless&width=681&height=908"
+    },
+    {
+        "name": "Itadori Yuji",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507871213706416278/Untitled130_20260523182144.png?ex=6a137a00&is=6a122880&hm=496c6e4e9474018eef917a2929b10e1c041e91f7d0d1e2ac0e21d6b3e3f521a9&=&format=webp&quality=lossless&width=681&height=908"
+    },
+    {
+        "name": "Zack Fair",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1507871559908462753/16_sin_titulo_20260524002307.png?ex=6a137a52&is=6a1228d2&hm=9b25848c316aa42fad4436431a8b7a896e1d1d242457f20a700efb8445996ea8&=&format=webp&quality=lossless&width=681&height=908"
+    },
+    {
+        "name": "Satoru Gojo",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://cdn.discordapp.com/attachments/997404068517515324/1508039763100172308/Untitled361_20260524160633.png?ex=6a1416f9&is=6a12c579&hm=a08118929017c1d8ad4048a64bfd552654f3a062e7e346e9499bb09718a407b6&"
+    },
+    {
+        "name": "Suguru Geto",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://cdn.discordapp.com/attachments/997404068517515324/1508039763892895825/Untitled361_20260524160806.png?ex=6a1416f9&is=6a12c579&hm=aa41a06fef0b551f2bfe120236996936764a8d60d6d1ad70b4939a10f5a6d6f6&"
+    },
+    {
+        "name": "Alma",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/997404068517515324/1508039864879157399/16_sin_titulo_20260524112515.png?ex=6a141711&is=6a12c591&hm=8316eb58dbdd967d989e4c9e988e382c623a4a2f4f31d21274ef910108128275&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Varka",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1508152339922686003/Untitled132_20260524125842.png?ex=6a147fd1&is=6a132e51&hm=2707266cc49f56ae665d885aef0c388fbe4d1151373f6b6af51072b70b473183&=&format=webp&quality=lossless&width=559&height=745"
+    },
+    {
+        "name": "Fushiguro Megumi",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://media.discordapp.net/attachments/1359253981590847893/1507871704716939324/Untitled130_20260523182342.png?ex=6a137a75&is=6a1228f5&hm=abf052b00d1b3e885d812dd16cdc7d7486bbbcdde2f48d9d912d14d1c38a3a29&=&format=webp&quality=lossless&width=681&height=908"
+    }
+ 
+
+
+
+
+]
