4863e370cf1e5d3cffa6b90f93ed1764923dbb8f
yvlvinaa <morenoyeleina@gmail.com>
2026-06-02 17:42:43 -0400

    Add files via upload

---
+++ cards (3).py
@@ -0,0 +1,378 @@
+cards = [
+    # ⭐⭐⭐ 3 STAR
+    {
+        "name": "Satoru Gojo",
+        "series": "Jujutsu Kaisen",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled361_20260524160540.webp?raw=true"
+    },
+    {
+        "name": "Satoru Gojo",
+        "series": "Jujutsu Kaisen",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/16_sin_titulo_20260602224348.webp?raw=true"
+    },
+    {
+        "name": "Jabber Wonger",
+        "series": "Gachiakuta",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled369_20260524200532.webp?raw=true"
+    },
+    {
+        "name": "Muichiro Tokito",
+        "series": "Kimetsu no Yaiba",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602232217.webp?raw=true"
+    },
+    {
+        "name": "Lighting Farron",
+        "series": "Final Fantasy",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled134_20260524182715.webp?raw=true"
+    },
+    {
+        "name": "Castorice",
+        "series": "Honkai: Star Rail",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled129_20260524125435.webp?raw=true"
+    },
+    {
+        "name": "Suguru Geto",
+        "series": "Jujutsu Kaisen",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled361_20260524160742.webp?raw=true"
+    },
+    {
+        "name": "Blade",
+        "series": "Honkai: Star Rail",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled129_20260524125335.webp?raw=true"
+    },
+    {
+        "name": "Mydei",
+        "series": "Honkai: Star Rail",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled359_20260523223351.webp?raw=true"
+    },
+    {
+        "name": "Zanka Nijiku",
+        "series": "Gachiakuta",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/Untitled369_20260524200253.webp?raw=true"
+    },
+    {
+        "name": "Hikaru",
+        "series": "The Summer Hikaru Died",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled369_20260524195427.webp?raw=true"
+    },
+    {
+        "name": "Till",
+        "series": "Alien Stage",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523221554.webp?raw=true"
+    },
+    {
+        "name": "Dewey",
+        "series": "Alien Stage",
+        "stars": 3,
+        "weight": 5,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled362_20260524083722.webp?raw=true"
+    },
+
+    # ⭐⭐ 2 STAR
+    {
+        "name": "Ivan",
+        "series": "Alien Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523221830.webp?raw=true"
+    },
+    {
+        "name": "Luka",
+        "series": "Alien Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523221839.webp?raw=true"
+    },
+    {
+        "name": "Giyu Tomioka",
+        "series": "Kimetsu no Yaiba",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602230952.webp?raw=true"
+    },
+    {
+        "name": "Uzui Tengen",
+        "series": "Kimetsu no Yaiba",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602231316.webp?raw=true"
+    },
+    {
+        "name": "Muichiro Tokito",
+        "series": "Kimetsu no Yaiba",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602231828.webp?raw=true"
+    },
+    {
+        "name": "Cloud Strife",
+        "series": "Final Fantasy",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled134_20260524182201.webp?raw=true"
+    },
+    {
+        "name": "Tsukasa Tenma",
+        "series": "Project Sekai: Colorful Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled131_20260524123802.webp?raw=true"
+    },
+    {
+        "name": "Shishikuno Mogari",
+        "series": "Phantom Busters",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524184203.webp?raw=true"
+    },
+    {
+        "name": "Clorinde",
+        "series": "Genshin Impact",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled132_20260524125656.webp?raw=true"
+    },
+    {
+        "name": "Enjin",
+        "series": "Gachiakuta",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/Untitled369_20260524201156.webp?raw=true"
+    },
+    {
+        "name": "Hinomori Shizuku",
+        "series": "Project Sekai: Colorful Stage!",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled131_20260524124848.webp?raw=true"
+    },
+    {
+        "name": "Lee Yeon",
+        "series": "Red Thread Quest",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524104433.webp?raw=true"
+    },
+    {
+        "name": "Hong Kihoon",
+        "series": "Red Thread Quest",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524105256.webp?raw=true"
+    },
+    {
+        "name": "Hyuna",
+        "series": "Alien Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523221932.webp?raw=true"
+    },
+    {
+        "name": "Sua",
+        "series": "Alien Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523221904.webp?raw=true"
+    },
+    {
+        "name": "Suguru Geto",
+        "series": "Jujutsu Kaisen",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260523235608.webp?raw=true"
+    },
+    {
+        "name": "Choso",
+        "series": "Jujutsu Kaisen",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524000923.webp?raw=true"
+    },
+    {
+        "name": "Zanka Nijiku",
+        "series": "Gachiakuta",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled369_20260524200305.webp?raw=true"
+    },
+    {
+        "name": "Shinonome Akito",
+        "series": "Project Sekai: Colorful Stage",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524000207.webp?raw=true"
+    },
+    {
+        "name": "Yoshiki",
+        "series": "The Summer Hikaru Died",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled369_20260524195328.webp?raw=true"
+    },
+    {
+        "name": "Tartaglia",
+        "series": "Genshin Impact",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled132_20260524125748.webp?raw=true"
+    },
+    {
+        "name": "Seishiro Nagi",
+        "series": "Bluelock",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524000611%20(1).webp?raw=true"
+    },
+    {
+        "name": "Zack Fair",
+        "series": "Final Fantasy",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524002307.webp?raw=true"
+    },
+    {
+        "name": "Itoshi Sae",
+        "series": "Bluelock",
+        "stars": 2,
+        "weight": 30,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled130_20260523181412.webp?raw=true"
+    },
+
+    # ⭐ 1 STAR
+    {
+        "name": "Mydei",
+        "series": "Honkai: Star Rail",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled359_20260523223439.webp?raw=true"
+    },
+    {
+        "name": "Mizi",
+        "series": "Alien Stage",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled357_20260523222147.webp?raw=true"
+    },
+    {
+        "name": "Riyo Reaper",
+        "series": "Gachiakuta",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled369_20260524200911.webp?raw=true"
+    },
+    {
+        "name": "Meguru Bachira",
+        "series": "Bluelock",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524001731.webp?raw=true"
+    },
+    {
+        "name": "Hyoma Chigiri",
+        "series": "Bluelock",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524001335%20(1).webp?raw=true"
+    },
+    {
+        "name": "Itadori Yuji",
+        "series": "Jujutsu Kaisen",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled130_20260523182144.webp?raw=true"
+    },
+    {
+        "name": "Satoru Gojo",
+        "series": "Jujutsu Kaisen",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled361_20260524160633.webp?raw=true"
+    },
+    {
+        "name": "Suguru Geto",
+        "series": "Jujutsu Kaisen",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled361_20260524160806.webp?raw=true"
+    },
+    {
+        "name": "Alma",
+        "series": "Gokurakugai",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260524112515.webp?raw=true"
+    },
+    {
+        "name": "Muichiro Tokito",
+        "series": "Kimetsu no Yaiba",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602231821.webp?raw=true"
+    },
+    {
+        "name": "Uzui Tengen",
+        "series": "Kimetsu no Yaiba",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602231304.webp?raw=true"
+    },
+    {
+        "name": "Giyu Tomioka",
+        "series": "Kimetsu no Yaiba",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/16_sin_titulo_20260602231026.webp?raw=true"
+    },
+    {
+        "name": "Malleus Draconia",
+        "series": "Twisted Wonderland",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled133_20260524170856.webp?raw=true"
+    },
+    {
+        "name": "Varka",
+        "series": "Genshin Impact",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled132_20260524125842.webp?raw=true"
+    },
+    {
+        "name": "Kafka",
+        "series": "Honkai: Star Rail",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled129_20260524125241.webp?raw=true"
+    },
+    {
+        "name": "Fushiguro Megumi",
+        "series": "Jujutsu Kaisen",
+        "stars": 1,
+        "weight": 60,
+        "image": "https://github.com/yvlvinaa/luka/blob/main/cards/Untitled130_20260523182342.webp?raw=true"
+    }
+]
